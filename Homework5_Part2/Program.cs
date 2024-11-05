0111f08df7aed7381346c7e8a8cda0ce5b5f9bf6
Emily Sierra <sierraemely@gmail.com>
2024-11-04 17:52:28 -0800

    Add project files.

---
+++ Homework5_Part2/Program.cs
@@ -0,0 +1,56 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] numbers = GenerateNumbers();
+        PrintNumbers(numbers);
+        Reverse(numbers);
+        PrintNumbers(numbers);
+    }
+
+    private static void PrintNumbers(int[] numbers)
+    {
+        Console.WriteLine("Elements in array: ");
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.WriteLine(numbers[i]);
+      
+        }
+    }
+
+    private static void Reverse(int[] numbers)
+    {
+        
+        for (int i = 0; i < numbers.Length / 2; i++)
+        {
+            //Swapping a and b
+            //int a;
+            //int b;
+            int temp;
+            //a = b;
+            //b = temp;
+            temp = numbers[i];
+            numbers[i] = numbers[numbers.Length - i - 1];
+            numbers[numbers.Length - i - 1] = temp;
+
+
+        }
+
+        Console.WriteLine("Reversed #s :");
+        return;
+
+    }
+
+    private static int[] GenerateNumbers()
+    {
+        int[] array = new int[10];
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.WriteLine(array[i]);
+            array[i] = int.Parse(Console.ReadLine());
+        }
+        return array;
+    }
+
+}
